a0056ec9c8f36e92681e7b97ba4bf59a37db7a85
notLucid <103009610+notLucidi@users.noreply.github.com>
2024-10-28 11:47:13 +0700

    Update transformer.lua

--- src/transformer.lua
+++ src/transformer.lua
@@ -1,1 +1,27 @@
+local Parser = require("parser")
+local Encoder = require("encoder")
 
+local Transformer = {}
+
+function Transformer:obfuscate(inputFile, outputFile)
+    local content = Parser:readFile(inputFile)
+
+    local encryptedContent, key = Encoder:encryptString(content)
+    
+    local obfuscatedCode = string.format([[
+local function decode(str, key)
+    -- [Masukkan fungsi dekripsi di sini]
+    -- Fungsi ini akan ditambahkan nanti
+end
+
+local encrypted = %q
+local key = %q
+local decoded = decode(encrypted, key)
+load(decoded)()
+    ]], encryptedContent, key)
+    
+    Parser:writeFile(outputFile, obfuscatedCode)
+    print("Obfuscation complete!")
+end
+
+return Transformer
