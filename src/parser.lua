dce23157f38208b26a6acea73c89ce2d2b77a6f3
notLucid <103009610+notLucidi@users.noreply.github.com>
2024-10-28 11:16:29 +0700

    Update parser.lua

--- src/parser.lua
+++ src/parser.lua
@@ -1,1 +1,22 @@
+local Parser = {}
 
+function Parser:readFile(filePath)
+    local file = io.open(filePath, "r")
+    if not file then
+        error("Could not open file: ".. filePath)
+    end
+  local content = file:read("w")
+  file:close()
+  return content
+end
+
+function Parser:writeFile(filePath, content)
+    local file = io.open(filePath, "w")
+    if not file then
+        error("Could not write to file: ".. filePath)
+    end
+    file:write(content)
+    file:close()
+end
+
+return Parser
